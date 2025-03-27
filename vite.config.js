f6ba733804fbf3777339bef985e472328a5d90e4
ABsievil <nccuong317qb@gmail.com>
2025-03-27 12:04:34 +0700

    feat(dockerfile): setup dockerfile and some deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      'https://examify-online.azurewebsites.net' // Add this line to allow the specific host
+    ],
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }
 })
